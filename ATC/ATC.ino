0bf0d610dbf69c8369d66fe1947746b8d8f20030
JTorode89 <jtorode89@gmail.com>
2017-06-14 15:54:39 +0800

    Unload & Load Functions
    functions added, still missing the necessary code to communicate with the existing cnc machine
    simplified current position update during rotation
    added precompiled directive variables

--- ATC/ATC.ino
+++ ATC/ATC.ino
@@ -1,17 +1,22 @@
+#define HOME_SENSE_PIN 12
+#define GENEVA_SENSE_PIN 13
+#define LIFT_CYLINDER_DELAY 5000
+#define PIVOT_CYLINDER_DELAY 10000
+
 int currentPosition;
-boolean homePosition;
-boolean toolInCNC;
-boolean genevaZero;   // needs a better name
+boolean homed = false;
+boolean toolInCNC = false;
+boolean genevaLocked = true;
 
 int lastLoaded;       // possibly irrelevant
 boolean ATCEngaged;   // possibly N/A
 boolean toolExists;   // possibily N/A
-boolean stock[] = {0,0,0,0,0,0,0,0);
+boolean stock[] = {0,0,0,0,0,0,0,0};
 
-const int motorSpdPin = a;    //PWM to H-Bridge
-const int motorDirPin = b;    //High/On is Clockwise
-const int pivotCylPin = c;
-const int liftCylPin = d;
+const int motorSpdPin = 1;    //PWM to H-Bridge
+const int motorDirPin = 2;    //High/On is Clockwise
+const int pivotCylPin = 3;
+const int liftCylPin = 4;
 
 void setup() {
   Serial.begin(9600);
@@ -19,19 +24,61 @@ void setup() {
   pinMode(motorDirPin, OUTPUT);
   pinMode(pivotCylPin, OUTPUT);
   pinMode(liftCylPin, OUTPUT);
-}
 
+  //toolInCNC = isCNCLoaded();
+  if (toolInCNC = true){
+    //insert error message that pauses the software until accepted
+  }
+  homing();
+}
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
 
 void homing(){
-  while (homePosition != true){
+  while (homed != true){
     rotateMotor(1,1); //clockiwise once
+    homed = digitalRead(HOME_SENSE_PIN);
   }
 
   currentPosition = 1;
+}
+
+void unloadTool(int reqTool){
+  //ask cnc if it's in position and if not make it so
+  digitalWrite(liftCylPin, HIGH);       //ATC:LIFT
+  delay(LIFT_CYLINDER_DELAY);     
+  digitalWrite(pivotCylPin, HIGH);      //ATC:IN
+  delay(PIVOT_CYLINDER_DELAY);
+  digitalWrite(liftCylPin, LOW);        //ATC:LOWER
+  //small delay here?
+  //tell cnc to engage the drawbar      //DRAWBAR:ON
+  delay(LIFT_CYLINDER_DELAY);       
+  //tell cnc to disengage the drawbar   //DRAWBAR:OFF
+  
+  if(reqTool != 0){
+    loadTool(reqTool);
+  }
+  else{ //if reqTool is 0 then there will be no unload, and ATC is finished
+    digitalWrite(pivotCylPin, LOW);
+    delay(PIVOT_CYLINDER_DELAY);        //ATC:OUT
+  }
+}
+
+void loadTool(int reqTool){
+   //ask cnc if it's in position and if not make it so
+   digitalWrite(pivotCylPin, HIGH);     //ATC:IN
+   delay(PIVOT_CYLINDER_DELAY);
+   rotatePath(reqTool);                 //ATC:ROTATE
+   //tell cnc to engage the drawbar     //DRAWBAR:ON
+   digitalWrite(liftCylPin, HIGH);      //ATC:LIFT
+   delay(LIFT_CYLINDER_DELAY);
+   //tell cnc to disengage the drawbar  //DRAWBAR:OFF
+   digitalWrite(pivotCylPin, LOW);      //ATC:OUT
+   delay(PIVOT_CYLINDER_DELAY);
+   digitalWrite(liftCylPin, LOW);       //ATC:LOWER
+   delay(LIFT_CYLINDER_DELAY);
 }
 
 void rotatePath(int endPos){
@@ -44,7 +91,7 @@ void rotatePath(int endPos){
   int rotDist;
 
   //tool positions are between 1 and 8 and the shortest path needs to be found
-  if (startPos > EndPos ){
+  if (startPos > endPos){
     down = startPos - endPos;
     up = endPos + 8 - startPos;
   }
@@ -71,26 +118,28 @@ void rotateMotor(int rotDir, int rotDist){
   //rotDist: the required number of positions to rotate on the geneva
   //rotDir: which direction the motor needs to turn the geneva
 
-  int n == 0;   //just to count up
-
   if (rotDir == 1){    //clockwise
-    digitalWrite(motorDirPin, HIGH)
+    digitalWrite(motorDirPin, HIGH);
   }
   else{             //anticlockwise
-    digitalWrite(motorDirPin, LOW)
+    digitalWrite(motorDirPin, LOW);
   }
 
   digitalWrite(motorSpdPin, HIGH);
   delay(1000);
   //this delay prevents the upcoming code from prematurely counting a successful loop due to detecting the sensor immediately
-  
+
+  int n = 0;
   while (n != rotDist){
-    if (genevaZero == true){
+    if (genevaLocked == true){
       n++;
-      currentPosition = currentPosition + (rotDir * 2 - 1);
-      //rotDir is 1 or 0 for clockwise or anticlockwise and the currentposition must move +1 or -1
-      //by having rotDir doubled minus one, the values 1 and 0 become 1 and -1 respectively
-     
+      if (rotDir == 1){ //if Direction clockwise position up 1
+        currentPosition++;
+      }
+      else{ //rotDir != 1: if direction anticlockwise position down 1
+        currentPosition--;
+      }
+      
       if (currentPosition == 9){
       currentPosition = 1; 
       }
