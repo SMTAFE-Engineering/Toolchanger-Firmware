f642f074fa5c61e67556ec6f00f6a2f5ea2f3a76
JTorode89 <jtorode89@gmail.com>
2017-06-14 14:13:15 +0800

    Homing & Rotate Path functions
    added the rotate path function which calculates the shortest path and runes the rotate motor function
    added the homing function intended for startup with a simple loop using rotate motor
    current position tracking added to rotatemotor using formula and converting positions 0 and 9 to 8 and 1 respectively
    rotatemotor while loop code functionality improved

--- ATC/ATC.ino
+++ ATC/ATC.ino
@@ -26,25 +26,87 @@ void loop() {
 
 }
 
-void rotateMotor(int dir, int dist){
-  //dist: the required number of positions to rotate
-  //dir: which direction the motor needs to turn
+void homing(){
+  while (homePosition != true){
+    rotateMotor(1,1); //clockiwise once
+  }
+
+  currentPosition = 1;
+}
+
+void rotatePath(int endPos){
+  //using the current and desired position, the shortest path needs to be calculated
+  
+  int startPos = currentPosition;
+  int down;
+  int up;
+  int rotDir;
+  int rotDist;
+
+  //tool positions are between 1 and 8 and the shortest path needs to be found
+  if (startPos > EndPos ){
+    down = startPos - endPos;
+    up = endPos + 8 - startPos;
+  }
+  else{ //ie endPos > startPos
+    up = endPos - startPos;
+    down = startPos + 8 - endPos;
+  }
+
+  //up equates to clockwise and thus the smaller determines the ideal direction
+  if (up < down){
+    rotDir = 1; //clockwise
+    rotDist = up;
+  }
+  else{ //ie down < up
+    rotDir = 0; //anticlock
+    rotDist = down;
+  }
+  
+  rotateMotor(rotDir, rotDist);
+}
+
+void rotateMotor(int rotDir, int rotDist){
+  //this function commands the motor to move the geneva in the desired direction, the desired number of rotations
+  //rotDist: the required number of positions to rotate on the geneva
+  //rotDir: which direction the motor needs to turn the geneva
 
   int n == 0;   //just to count up
 
-  if (dir == 1){    //clockwise
+  if (rotDir == 1){    //clockwise
     digitalWrite(motorDirPin, HIGH)
   }
   else{             //anticlockwise
     digitalWrite(motorDirPin, LOW)
   }
 
-  while (n != dist){
-    while ( genevaZero != true){
-      digitalWrite(motorSpdPin, HIGH)
+  digitalWrite(motorSpdPin, HIGH);
+  delay(1000);
+  //this delay prevents the upcoming code from prematurely counting a successful loop due to detecting the sensor immediately
+  
+  while (n != rotDist){
+    if (genevaZero == true){
+      n++;
+      currentPosition = currentPosition + (rotDir * 2 - 1);
+      //rotDir is 1 or 0 for clockwise or anticlockwise and the currentposition must move +1 or -1
+      //by having rotDir doubled minus one, the values 1 and 0 become 1 and -1 respectively
+     
+      if (currentPosition == 9){
+      currentPosition = 1; 
       }
-    digitalWrite(motorSpdPin, LOW)
-    n++;
+      if (currentPosition == 0){
+      currentPosition = 8;
+      }
+      //loop constraint, when position becomes 0 or 9 it is correctly adjusted to 8 or 1
+      
+      if (n != rotDist){
+        delay(1000);
+      }
+      //delay is to prevent counting more than once each time the sensor is detected
+      //the if statement prevents the motor overshooting on the final loop because the motor stop is outside the loop
+    }
   }
+  
+  digitalWrite(motorSpdPin, LOW);
 }
 
