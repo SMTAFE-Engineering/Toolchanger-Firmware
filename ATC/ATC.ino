3a970ddbb6e5f943e2500f0cdc1e2632c9fdc394
JTorode89 <jtorode89@gmail.com>
2017-06-16 11:14:01 +0800

    successful blinky switchboard test
    created input menus via switch command which successfully control a blinky light via relay as expected

--- ATC/ATC.ino
+++ ATC/ATC.ino
@@ -1,11 +1,17 @@
-#define HOME_SENSE_PIN 12
-#define GENEVA_SENSE_PIN 13
 #define LIFT_CYLINDER_DELAY 5000
 #define PIVOT_CYLINDER_DELAY 10000
+
+#define HOME_SENSE_PIN 12
+#define GENEVA_SENSE_PIN 14
 #define MOTOR_SPEED_PIN 1           //PWM to H-Bridge
 #define MOTOR_DIRECTION_PIN 2       //High = Clockwise
 #define PIVOT_CYLINDER_PIN 3
 #define LIFT_CYLINDER_PIN 4
+
+#define TEST_PIN_A 10
+#define TEST_PIN_B 13
+int incomingByte;
+int testModeActive = 0;
 
 int currentPosition;
 boolean homed = false;
@@ -23,45 +29,178 @@ void setup() {
   pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
   pinMode(PIVOT_CYLINDER_PIN, OUTPUT);
   pinMode(LIFT_CYLINDER_PIN, OUTPUT);
-
+  
+  pinMode(TEST_PIN_A, OUTPUT);
+  pinMode(TEST_PIN_B, OUTPUT);
+  digitalWrite(TEST_PIN_B, LOW);
+  
   //toolInCNC = isCNCLoaded();
   if (toolInCNC = true){
-    //insert error message that pauses the software until accepted
+    //insert error message that pauses the software until dealt with
   }
-  homing();
+  //homing();
 }
 void loop() {
-  // put your main code here, to run repeatedly:
+  //awaits user input to switch a tool
+  //valid inputs are: 0, 1 through 8, and T
+  //0 just causes an unload
+  //1 through 8 requests a tool which may require unloading first
+  //T which sends the user into Test Mode allowing further commands
 
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+
+    int b = 0; //blinky light for testing, used in loop below
+
+    int requestedToolID = 0;
+
+    if (testModeActive == 0){
+      //default menu
+      switch(incomingByte){
+        case 'T':{
+          testModeActive = 1;
+          digitalWrite(TEST_PIN_B, HIGH);
+          break;
+        }
+        case '0':{
+          requestedToolID = 0;
+          break;
+        }
+        case '1':{
+          requestedToolID = 1;
+          break;
+        }
+        case '2':{
+          requestedToolID = 2;
+          break;
+        }
+        case '3':{
+          requestedToolID = 3;
+          break;
+        }
+        case '4':{
+          requestedToolID = 4;
+          break;
+        }
+        case '5':{
+          requestedToolID = 5;
+          break;
+        }
+        case '6':{
+          requestedToolID = 6;
+          break;
+        }
+        case '7':{
+          requestedToolID = 7;
+          break;
+        }
+        case '8':{
+          requestedToolID = 8;
+          break;
+        }
+      }
+      b = requestedToolID;
+    }
+    else{
+      //test menu
+      switch(incomingByte){
+        case 'X':{
+          testModeActive = 0;
+          digitalWrite(TEST_PIN_B, LOW);
+          break;
+        }
+        case 'U':{
+          b = 2;
+          break;
+        }
+        case 'D':{
+           b = 3;
+           break;
+        }
+        case 'I':{
+          b = 4;
+          break;
+          }
+        case 'O':{
+          b = 5;
+          break;
+        }
+        case 'R':{
+          b = 6;
+          break;
+        }
+        case 'L':{
+          b = 7;
+          break;
+        }
+      }
+    }
+
+    if (b != 0){
+      for (int n = 0; n != b; n++){
+        digitalWrite(TEST_PIN_A, HIGH);
+        delay(250);
+        digitalWrite(TEST_PIN_A, LOW);
+        delay(250);
+      }  
+    }
+  }
+}
+
+void testMode(){
+  //await user input
+  //X returns the user to prior menu
+  //U commands the atc to lift the current tool UP
+  //D commands the atc to lower the current tool DOWN
+  //I commands the atc to move IN and engage with the CNC
+  //O commands the atc to move OUT and disengage with the CNC
+  //R commands the geneva to rotate clockwise once
+  //L commands the geneva to rotate anti-clockwise once
 }
 
 void homing(){
   while (homed != true){
-    rotateMotor(1,1); //clockiwise once
+    rotateGenevaClockwiseOnce();
     homed = digitalRead(HOME_SENSE_PIN);
   }
-
   currentPosition = 1;
+}
+
+void rotateGenevaClockwiseOnce(){
+  rotateMotor(1,1); 
+  //(clockwise = 1, distance = 1)
+}
+
+void rotateGenevaAnticlockwiseOnce(){
+  rotateMotor(0,1);
+  //(anticlockwise = 0, distance = 1)
+}
+
+void switchTool(int reqTool){
+  //if the CNC has a tool in, then the CNC needs to be unloaded before a new tool can be loaded up
+  if (toolInCNC == true){
+    unloadTool(reqTool);
+  }
+  loadTool(reqTool);
 }
 
 void unloadTool(int reqTool){
   //ask cnc if it's in position and if not make it so
-  digitalWrite(LIFT_CYLINDER_PIN, HIGH);       //ATC:LIFT
+  digitalWrite(LIFT_CYLINDER_PIN, HIGH);     //ATC:LIFT
   delay(LIFT_CYLINDER_DELAY);     
-  digitalWrite(PIVOT_CYLINDER_PIN, HIGH);      //ATC:IN
+  digitalWrite(PIVOT_CYLINDER_PIN, HIGH);    //ATC:IN
   delay(PIVOT_CYLINDER_DELAY);
-  digitalWrite(LIFT_CYLINDER_PIN, LOW);        //ATC:LOWER
+  digitalWrite(LIFT_CYLINDER_PIN, LOW);     //ATC:LOWER
   //small delay here?
-  //tell cnc to engage the drawbar      //DRAWBAR:ON
+  //tell cnc to engage the drawbar          //DRAWBAR:ON
   delay(LIFT_CYLINDER_DELAY);       
-  //tell cnc to disengage the drawbar   //DRAWBAR:OFF
-  
-  if(reqTool != 0){
-    loadTool(reqTool);
-  }
-  else{ //if reqTool is 0 then there will be no unload, and ATC is finished
+  //tell cnc to disengage the drawbar       //DRAWBAR:OFF
+
+  //if required tool is ID 0 then only an unload is required, thus the ATC will not want to remain engaged
+  if(reqTool == 0){
     digitalWrite(PIVOT_CYLINDER_PIN, LOW);
-    delay(PIVOT_CYLINDER_DELAY);        //ATC:OUT
+    delay(PIVOT_CYLINDER_DELAY);            //ATC:OUT
   }
 }
 
@@ -69,11 +208,11 @@ void loadTool(int reqTool){
    //ask cnc if it's in position and if not make it so
    digitalWrite(PIVOT_CYLINDER_PIN, HIGH);     //ATC:IN
    delay(PIVOT_CYLINDER_DELAY);
-   rotatePath(reqTool);                 //ATC:ROTATE
-   //tell cnc to engage the drawbar     //DRAWBAR:ON
+   rotatePath(reqTool);                        //ATC:ROTATE
+   //tell cnc to engage the drawbar            //DRAWBAR:ON
    digitalWrite(LIFT_CYLINDER_PIN, HIGH);      //ATC:LIFT
    delay(LIFT_CYLINDER_DELAY);
-   //tell cnc to disengage the drawbar  //DRAWBAR:OFF
+   //tell cnc to disengage the drawbar         //DRAWBAR:OFF
    digitalWrite(PIVOT_CYLINDER_PIN, LOW);      //ATC:OUT
    delay(PIVOT_CYLINDER_DELAY);
    digitalWrite(LIFT_CYLINDER_PIN, LOW);       //ATC:LOWER
